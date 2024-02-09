a0ae101cf4c69f2539d3fc7c875e8fb3650be1e0
gharieb khalifa <blacksnow.soon@gmail.com>
2024-02-09 11:39:25 +0200

    done with contact form

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -11,7 +11,7 @@ function ContactForm() {
 
 
   const onSubmit = (values, actions) => {
-    console.log('actions', actions)
+  
     const service_id = 'service_gh_mailer'
     const template_id = 'contact_form'
     const publicKey = 'kW68NtHPmP4o2a8mL'
@@ -21,15 +21,15 @@ function ContactForm() {
       message: values?.message,
       subject: values?.subject
     }
+    
     emailjs.send(service_id, template_id,emailTemplate, publicKey)
-    .then(res => {
-      console.log('respons', res)
-      actions.setStatus(...res)
-      actions.setSubmitting(false)
+    .then(() => {
       actions.resetForm()
+      actions.setStatus('submitted')
+      actions.setSubmitting(false)
     })
     .catch(err => {
-      actions.setErrors(...err)
+      actions.setErrors(err)
       actions.setSubmitting(false)
     })
   }
@@ -92,13 +92,12 @@ const SubmitBtn = () => {
 
   const {isSubmitting, status, errors } = useFormikContext()
   
-  console.log('status', status)
   return (
     <div className='flex items-center gap-1 duration-700 flex-col'>
       <button 
         type='submit' 
         className='w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
-        disabled={isSubmitting}
+        disabled={isSubmitting || status === 'submitted'}
         >
         {
           isSubmitting ? 
@@ -106,16 +105,19 @@ const SubmitBtn = () => {
             <span className='animate-spin w-4 h-4 block border-4 rounded-full mx-4 border-dashed border-cyan-100'></span>
             <span>sending...</span>
           </i>
-          : 'Submit'
+          : status === 'submitted' ?
+          <p className='text-green-500'>
+            Thank you !! I will Replay you Soon ;-)  
+          </p> 
+          :
+          'Submit'
         }
         
       </button>
       {
         errors ? <p className='text-red-500'></p> : null
       }
-      {
-        status ? <p className='text-red-500'></p> : null
-      }
+      
     </div>
   )
 }
