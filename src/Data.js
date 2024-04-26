4e4ce19b34036565e59cb9dd03683c29fa29091a
gharieb khalifa <blacksnow.soon@gmail.com>
2024-04-26 16:13:40 +0300

    adding more date to the projects list

--- src/Data.js
+++ src/Data.js
@@ -4,11 +4,11 @@ export const PROJECTS = [
   {
     id:1,
     name: 'Casher Bill',
-    image: 'bill-375x475.png',
-    shorts: 'A small app for a adabia casher bill includes some queries and reports, built from scratch.',
+    image: 'bill1-375x475.png',
+    shorts: 'A small web app for the casher of adabia port include login system, queries and reports, built from scratch.',
     description: 'Analyising the casher cycle and build an app working locally using indexed DB with all features as login system, multi users, reports, printing bills, export bills as PDF, export reports as CSV ',
     usedLibs: ['Vitejs-react', 'Dexie-js', 'date-fns', 'Jspdf', 'React-csv', 'React-datepicker', 'React-hook-form', 'html2canvas'],
-    functions: ['Login users', 'printing', 'Querying Data from DB', 'exporting Data as CSV & PDF', 'form validation', 'apply animations', 'CRUD'],
+    functions: ['authentication', 'printing', 'Querying Data from DB', 'exporting Data as CSV & PDF', 'form validation', 'apply animations', 'all CRUD operations'],
     link: 'https://casher-bill.vercel.app/'
   },
   {
@@ -28,7 +28,7 @@ export const PROJECTS = [
     shorts: 'A website for small aluminum workshop to be as a protfolio and catalogue.',
     description: 'A small website that has a dashoard lets the owner to add his content with full control plus some statistics with a good UI that lets users signin and save theme favorate kitchens ability to contact the brand with whats app or via direct call.',
     usedLibs: ['reactjs', 'firebase', 'firestore', 'firebase storage', 'prop-types', 'react-query', 'react-hook-form'],
-    functions: ['google Signin', 'dashboard', 'upload images', 'save user data', 'paginate', 'firebase hosting', 'authorization'],
+    functions: ['google Signin', 'creating admin dashboard','reading files', 'upload images', 'validating forms', 'save user data', 'paginate', 'firebase hosting', 'authorization'],
     link: 'https://workshop-full-website.vercel.app/',
   },
   {
@@ -37,7 +37,7 @@ export const PROJECTS = [
     image: 'landing-375x475.png',
     shorts: 'build a landing responsive page with a dynamic navigation bar based on created sections in the page',
     description: 'It is static responsve page with an dynamic nav bar creates a links to all the sections in the page based on the section tag with the id assign a home link to the hero section and footer to the footer',
-    usedLibs: ['no Libs just js vanila', 'css', 'html'],
+    usedLibs: ['no Libs just vanila js ', 'css', 'html'],
     functions: ['responsive design', 'dynamic nav bar', 'manipulating DOM'],
     link: 'https://landing-page-udacity-fwd.vercel.app/',
   },
@@ -47,9 +47,9 @@ export const PROJECTS = [
     image: 'api-375x475.png',
     shorts: 'This API allows you to resize images by specifying the desired width and height as a search params and a payload is the image base64 format',
     description: 'The Image Reduce API provides a simple and convenient way to resize images by providing the desired width and height along with the base64 encoded image file',
+    usedLibs: ['Nodejs', 'sharpe', 'express', 'cors', 'jasmine', 'body-parser'],
+    functions: ['creating the express server', 'optimize cors options & body-pareser','adding routes'],
     link: 'https://github.com/blacksnowsoon/Image-Reducer-Api',
-    usedLibs: ['Nodejs', 'sharpe', 'express', 'cors'],
-    functions: ['reading files', ],
   },
   {
     id: 6,
@@ -58,8 +58,8 @@ export const PROJECTS = [
     shorts: 'Simple web app to resize images by providing the desired width and height along with the image file',
     description: 'The Image Reducer Web App provides a simple and convenient way to resize images by providing the desired width and height along with the image file',
     link: 'https://image-reducer-ui.vercel.app/',
-    usedLibs: ['Reactjs', 'axios'],
-    functions: ['responsive design', 'dark mode', 'scroll up', 'animation', 'validating input', 'fetching data', 'Submting data'],
+    usedLibs: ['React', 'axios'],
+    functions: ['responsive design', 'display mode', 'scroll up', 'animation', 'validating input', 'fetching data', 'Submting data'],
   }
 ]
 
