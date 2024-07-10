daee4f75c5943fc2bda4342ce40119c8669db31d
gharieb khalifa <blacksnow.soon@gmail.com>
2024-07-11 01:44:44 +0300

    delete unused code

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -22,7 +22,6 @@ module.exports = {
       },
       animation: {
         'pass': 'pass 10s infinite linear',
-        'pull-up' : 'pull-up 15s forwards linear',
         'in-out': 'in-out .5s infinite alternate-reverse'
       },
       keyframes: {
@@ -30,28 +29,9 @@ module.exports = {
           '0%': { transform: 'translateX(100%)' },
           '100%': { transform: 'translateX(-150%)' }
         },
-        'pull-up': {
-          "0%": {
-            'transform-origin': 'top right',
-            'transform': 'rotate(0deg)',
-            '-webkit-animation-timing-function': 'ease-out',
-            'animation-timing-function': 'ease-out',
-          },
-          "100%": {
-            'transform-origin': 'top right',
-            'transform': 'rotate(-180deg)',
-            '-webkit-animation-timing-function': 'ease-in-out',
-            'animation-timing-function': 'ease-in-out',
-          }
-        },
-        'in-out': {
-          '0%': { transform: 'translateX(0)' },
-          '100%': { transform: 'translateX(50%)' }
-        }
+        
       },
-      bgGradientDeg: {
-        'background-image': `conic-gradient(from 0.25turn at var(--increase)% var(--decrease)%, var(--tw-gradient-to), 180deg, var(--tw-gradient-to), 180deg, var(--tw-gradient-from))`
-      },
+      
     },
   },
   daisyui: {
