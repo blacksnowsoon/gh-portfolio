585e16e37fe6e5a73cc70451f7e8986149f663a6
gharieb khalifa <blacksnow.soon@gmail.com>
2024-08-31 17:56:23 +0300

    fix error comment uncommented

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -25,4 +25,4 @@ function Header() {
 }
 
 export default Header
-[background:linear-gradient(45deg,theme(colors.slate.800)_50%,theme(colors.slate.800)_50%,#172033)_padding-box,conic-gradient(from_var(--border-angle),theme(colors.slate.600/.48)_80%,_theme(colors.indigo.500)_86%,_theme(colors.indigo.300)_90%,_theme(colors.indigo.500)_94%,_theme(colors.slate.600/.48))_border-box]
+// [background:linear-gradient(45deg,theme(colors.slate.800)_50%,theme(colors.slate.800)_50%,#172033)_padding-box,conic-gradient(from_var(--border-angle),theme(colors.slate.600/.48)_80%,_theme(colors.indigo.500)_86%,_theme(colors.indigo.300)_90%,_theme(colors.indigo.500)_94%,_theme(colors.slate.600/.48))_border-box]
